79b98c3f9699aac2357a84c5210033f0af6a7b23
John D. Johnson <jdjohn215@gmail.com>
2020-01-07 14:55:20 -0600

    init

---
+++ R/BuildAllVariableNames.R
@@ -0,0 +1,28 @@
+# This file creates the variable code list
+library(tidyverse)
+library(tidycensus)
+
+get_acs <- function(year){
+  load_variables(year, dataset = "acs5") %>%
+    separate(col = "name", into = c("table", "variable"), remove = FALSE, sep = "_") %>%
+    select(-variable) %>%
+    mutate(year = year)
+}
+
+get_sf1 <- function(year){
+  load_variables(year, "sf1") %>%
+    separate(col = "name", into = c("table", "variable"), remove = FALSE, sep = "_") %>%
+    select(-variable) %>%
+    mutate(year = year,
+           type = "sf1")
+}
+
+acs.vars <- map(2010:2018, get_acs) %>%
+  bind_rows()
+
+sf1.vars <- map(c(1990,2000,2010), get_sf1) %>%
+  bind_rows()
+
+all.vars <- bind_rows(acs.vars, sf1.vars)
+
+usethis::use_data(all.vars)
